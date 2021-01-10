935b93e7ae63ab115bdf472a406d7d7858f331d7
Selena Siri <selenasiri@gmail.com>
2021-01-09 23:40:36 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Greeting from './Greeting'
+
+const App = () => {
+  return (
+    <div>
+      <Greeting name="Harry" />
+      <iframe src="https://giphy.com/embed/26BRzozg4TCBXv6QU" width="480" height="271" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/inpulsedm-26BRzozg4TCBXv6QU"></a></p>
+    </div>
+  )
+}
+
+export default App
