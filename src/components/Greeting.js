d35017493f4e3f595154fcdf80bfe419e228ad29
Selena Siri <selenasiri@gmail.com>
2021-01-12 21:45:27 -0500

    Greeting

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Greeting = (props) => {
   return(
     <div>
-      <h1>Hello {props.name}</h1>
+      <h1>Hello {props.name}, Adventure awaits!</h1>
     </div>
   )
 }
